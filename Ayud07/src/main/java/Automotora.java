b0c7c6ce919b9cd564f259dabc53438ec0c8df9b
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-11-12 23:29:03 -0300

    Actualización: corrección de clases

---
+++ Ayud07/src/main/java/Automotora.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Automotora {
+
+	private Collection<Vehiculo> vehiculos;
+	private List<Vehiculo> listaVehiculos;
+
+	public List<Vehiculo> getListaVehiculos() {
+		return this.listaVehiculos;
+	}
+
+	/**
+	 * 
+	 * @param listaVehiculos
+	 */
+	public void setListaVehiculos(List<Vehiculo> listaVehiculos) {
+		this.listaVehiculos = listaVehiculos;
+	}
+
+	/**
+	 * 
+	 * @param vehiculo
+	 */
+	public boolean agregarVehiculo(Vehiculo vehiculo) {
+		// TODO - implement Automotora.agregarVehiculo
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean listarVehiculos() {
+		// TODO - implement Automotora.listarVehiculos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param vehiculo
+	 */
+	public boolean mostrarDetalles(Vehiculo vehiculo) {
+		// TODO - implement Automotora.mostrarDetalles
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Ayud07/src/main/java/GestorDeDatos.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class GestorDeDatos {
+
+	public List<Vehiculo> leerVehiculos() {
+		// TODO - implement GestorDeDatos.leerVehiculos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param vehiculos
+	 */
+	public void guardarVehiculos(List<Vehiculo> vehiculos) {
+		// TODO - implement GestorDeDatos.guardarVehiculos
+		throw new UnsupportedOperationException();
+	}
+
+}
